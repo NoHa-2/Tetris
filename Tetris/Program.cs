4f23ae5810dc18434b4f06fdc8fd9cdfee92bb3d
NoHa <gamerstroveirl@gmail.com>
2025-11-24 17:10:50 +0800

    added new methods for tetromino class

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,19 +1,45 @@
-﻿using Tetris;
+﻿using System;
+using Tetris;
 
 namespace Program
 {
     class Program
     {
+        static readonly int startingBorderX = 30;
+        static readonly int endBorderX = 41;
+        static readonly int startingBorderY = 5;
+        static readonly int endBorderY = 25;
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            var buffer = new ConsoleBuffer();
-            buffer.ClearBuffer();
+            ConsoleBuffer.frameBuffer.ClearBuffer();
+            Tetrominos.GetNextTetromino();
+            Tetrominos.GetCurrentTetromino();
+            Tetrominos.DrawTetromino();
+            DrawBorder();
 
+
+
+
+
+            ConsoleBuffer.frameBuffer.Draw();
+            Console.Read();
 
             // game logic
 
             // display stuff
+        }
+        public static void DrawBorder()
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                ConsoleBuffer.frameBuffer.WriteCharToBuffer('#', startingBorderX, startingBorderY + i);
+                ConsoleBuffer.frameBuffer.WriteCharToBuffer('#', endBorderX, startingBorderY + i);
+            }
+            for (int i = 0; i < 11; i++)
+            {
+                ConsoleBuffer.frameBuffer.WriteCharToBuffer('#', startingBorderX + i, endBorderY);
+            }
         }
     }
 }
--- Tetris/Tetrominos.cs
+++ Tetris/Tetrominos.cs
@@ -8,6 +8,24 @@ namespace Tetris
 {
     class Tetrominos
     {
+        private static Random rnd = new Random();
+
+        private static readonly int tetrominoSpawnX = 35;
+        private static readonly int tetrominoSpawnY = 5;
+
+        //current tetromino coords
+        /*
+        public int currentTetrominoBlock1X {  get; set; }
+        public int currentTetrominoBlock1Y { get; set; }
+        public int currentTetrominoBlock2X { get; set; }
+        public int currentTetrominoBlock2Y { get; set; }
+        public int currentTetrominoBlock3X { get; set; }
+        public int currentTetrominoBlock3Y { get; set; }
+        public int currentTetrominoBlock4X { get; set; }
+        public int currentTetrominoBlock4y { get; set; }
+        */
+        public int[,] currentTetrominoCoords = new int[4, 2];
+
         // tetrominos
         private static char[,] shapeO = { {'■', '■'}, {'■', '■'} };
         private static char[,] shapeZ = { {'■', '■', ' '}, {' ', '■', '■'} };
@@ -17,6 +35,35 @@ namespace Tetris
         private static char[,] shapeS = { {' ', '■', '■'}, {'■', '■', ' ' } };
         private static char[,] shapeI = { { '■', '■', '■', '■' } };
 
-        public static List<char[,]> TetrominoBag = new() {shapeO, shapeZ, shapeL, shapeJ, shapeT, shapeS, shapeI};
+        public static char[,] nextTetromino { get; set; }
+        public static char[,] currentTetromino { get; set; }
+        public static List<char[,]> TetrominoBag = new() { shapeO, shapeZ, shapeL, shapeJ, shapeT, shapeS, shapeI };
+
+        public static void GetNextTetromino()
+        {
+            int randomint = rnd.Next(0,6);
+            nextTetromino = TetrominoBag[randomint];
+        }
+        public static void GetCurrentTetromino()
+        {
+            currentTetromino = nextTetromino;
+        }
+        public static void DrawTetromino()
+        {
+            
+            for (int i  = 0; i < currentTetromino.Length;)
+            {
+                if (currentTetromino[i, 1] == ' ')
+                {
+                    continue;
+                }
+                else
+                {
+                    ConsoleBuffer.frameBuffer.WriteCharToBuffer(block, tetrominoSpawnX, tetrominoSpawnY - i);
+                    i++;
+                }
+            }
+            
+        }
     }
 }
